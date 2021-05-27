359c7daf2fe7876f1da404a53e7cc21581bbf47c
Yauheniyaliashkevich <yauheniyaliashkevich@gmail.com>
2021-05-27 15:20:25 -0500

    Framework

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,5 +1,24 @@
 package pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.CommonMethods;
+
+public class LoginPage extends CommonMethods{
+
+    @FindBy (id="txtUsername")
+    public WebElement usernameBox;
+
+    @FindBy (id="txtPassword")
+    public WebElement passwordBox;
+
+    @FindBy (id="btnLogin")
+    public WebElement loginBtn;
+
+    public LoginPage (){
+        PageFactory.initElements(driver, this);
+    }
 
 }
--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -1,2 +1,28 @@
-package testcases;public class LoginTest {
+package testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.DashBoardPage;
+import pages.LoginPage;
+import utils.CommonMethods;
+import utils.ConfigReader;
+
+public class LoginTest extends CommonMethods {
+
+    @Test
+    public void adminLogin(){
+
+        LoginPage loginPage=new LoginPage();
+        sendTest(loginPage.usernameBox, ConfigReader.getPropertiesValue("username"));
+        sendTest(loginPage.passwordBox, ConfigReader.getPropertiesValue("password"));
+        click(loginPage.loginBtn);
+
+        //validation
+        //assertion
+
+        DashBoardPage dashBoardPage=new DashBoardPage();
+        Assert.assertTrue(dashBoardPage.welcomeMessage.isDisplayed(), "welcome message is not displayed");
+    }
+
+
 }
